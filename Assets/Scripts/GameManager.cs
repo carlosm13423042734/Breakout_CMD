db6ab3ed9319a759989112c2e2d17843f81967a6
Carlos Martínez <carlosssmrtzz@gmail.com>
2024-11-14 18:16:04 +0100

    instance

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,6 +7,7 @@ public class GameManager : MonoBehaviour
     [SerializeField]
     private int lives;
     private int bloquesRestantes;
+    public static GameManager Instance { get; private set; }
 
     public int Lives { get { return lives; } }
     public int BloquesRestantes { get; set; }
